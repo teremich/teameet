6c3f2069f71cfc85298d8540c13407b228ff9fbe
teremich <michi.tereschenko@gmail.com>
2022-08-14 19:14:02 +0200

    decided to not include the built front end

--- public/js/648.b5459506.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[648],{6648:function(n,e,t){t.r(e),t.d(e,{default:function(){return f}});var r=t(5288);const u=(0,r._)("p",null,"sorry, we couldn't find this page",-1),c=[u];function i(n,e){return(0,r.wg)(),(0,r.iD)("div",null,c)}var l=t(7184);const s={},a=(0,l.Z)(s,[["render",i]]);var f=a}}]);
-//# sourceMappingURL=648.b5459506.js.map
