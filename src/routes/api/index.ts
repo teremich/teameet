c22fa65315422aede3be78b0dcc6fadf9a500518
teremich <michi.tereschenko@gmail.com>
2022-08-26 13:11:28 +0200

    started versioning api

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,37 +1,9 @@
 import { Router } from "express";
-import type { Request, Response } from "express";
-import { requireAuth, level } from "middleware/auth";
-
-import * as auth from "./auth";
-import * as profile from "./profile";
-import * as project from "./project";
-import * as join from "./join";
+// import type { Request, Response } from "express";
+import * as v0 from "./v0";
+// import * as v1 from "./v1";
 
 export const router = Router();
 
-// /login /register
-router.use(auth.router);
-// /profile
-router.use(profile.router);
-// /project
-router.use(project.router);
-// /join
-router.use(join.router);
-
-
-router.route("/")
-    .all(requireAuth(level.ADMIN))
-    .get((req: Request, res: Response) => {
-        res.send("get api");
-    })
-    .post((req: Request, res: Response) => {
-        res.send("post api");
-    })
-    .put((req: Request, res: Response) => {
-        res.send("put api");
-    })
-    .delete((req: Request, res: Response) => {
-        res.send("delete api");
-    });
-
-
+router.use("/v0", v0.router);
+// router.use();
---
+++ src/routes/api/v0/index.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import type { Request, Response } from "express";
+import { requireAuth, level } from "middleware/auth";
+
+import * as auth from "./auth";
+import * as profile from "./profile";
+import * as project from "./project";
+import * as join from "./join";
+
+export const router = Router();
+
+// /login /register
+router.use(auth.router);
+// /profile
+router.use(profile.router);
+// /project
+router.use(project.router);
+// /join
+router.use(join.router);
+
+
+router.route("/")
+    .all(requireAuth(level.ADMIN))
+    .get((req: Request, res: Response) => {
+        res.send("get api");
+    })
+    .post((req: Request, res: Response) => {
+        res.send("post api");
+    })
+    .put((req: Request, res: Response) => {
+        res.send("put api");
+    })
+    .delete((req: Request, res: Response) => {
+        res.send("delete api");
+    });
+
+
